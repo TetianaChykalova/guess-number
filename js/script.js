2bb57356fba3df5a9d1ca2848fed3bbde69c65ac
Tetiana Chykalova <chikalova.t@gmail.com>
2022-12-02 11:56:55 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//VARIABLES TEXT
+let titleNumber = document.querySelector('.title__ask-number').textContent
+let inputValue = document.querySelector('.content__input-input').value
+let attemptsNumber = document.querySelector('.content__text-attempt').textContent
+let bestResult = document.querySelector('.content__text-result').textContent
+
+//VARIABLES BUTTON
+const buttonStart = document.querySelector('.button-start')
+const buttonTry = document.querySelector('.content__input-button')
+
+
